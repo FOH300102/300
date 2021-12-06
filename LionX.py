86302839c21f43e7d8144d5c0b093eeead3fbfdd
FOH300102 <90313452+FOH300102@users.noreply.github.com>
2021-12-06 16:49:08 +0700

    Update LionX.py

--- LionX.py
+++ LionX.py
@@ -6,8 +6,8 @@ logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
 )
 # Starting bot
-system("git clone https://gitHub.com/TeamLionX/LionX && cd LionX && python3 -m userbot")
+system("git clone https://gitHub.com/FOH300102/300 && cd 300 && python3 -m userbot")
 # if got error
-print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @LionXsupport")
+print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @marikitamerapat")
 # Script written for stop heroku suspension
 # use with credit else gay
